e4f9e2e270bd51cee6768a37b6a08aef10659e88
LinkedList <martin.macko@productboard.com>
2023-09-11 12:30:18 +0200

    Initial commit with test method

---
+++ src/main/java/com/productboard/Main.java
@@ -0,0 +1,11 @@
+package com.productboard;
+
+public class Main {
+    public static void main(String[] args) {
+        test();
+    }
+    
+    public static void test() {
+        throw new RuntimeException("To be implemented according to the assignment");
+    }
+}
